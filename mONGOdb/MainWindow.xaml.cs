3356c20ba147a5e97bd859ba51ea79878dde8bfe
Daniel Lins Maciel <dan.8p@hotmail.com>
2020-11-04 19:18:09 -0300

    Initial commit

---
+++ mONGOdb/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mONGOdb
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            IMongoClient client = new MongoClient("mongodb://localhost");
+            IMongoDatabase database = client.GetDatabase("example");
+            IMongoCollection<Postagem> colNews = database.GetCollection<Postagem>("news");
+
+
+            //criar
+            Postagem doc = new Postagem();
+            doc.Title = "Titulo";
+            doc.Body = "Texto";
+            doc.Criado = DateTime.Now;
+            doc.Active = true;
+            colNews.InsertOne(doc);
+
+
+            //pesquisar
+            Expression<Func<Postagem, bool>> filter =
+                x => x.Id.Equals(ObjectId.Parse("594b093325841c1b6cac28ea"));
+
+            Postagem news = colNews.Find(filter).FirstOrDefault();
+            if (news != null)
+            {
+                news.Value = 200d;
+                ReplaceOneResult result = colNews.ReplaceOne(filter, news);
+            }
+
+            //apagar
+            filter = x => x.Id.Equals(ObjectId.Parse("594b093325841c1b6cac28ea"));
+            DeleteResult delresult = colNews.DeleteOne(filter);
+        }
+    }
+}
---
+++ mONGOdb/Postagem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace mONGOdb
+{
+        public class Postagem
+        {
+            [BsonId()]
+            public ObjectId Id { get; set; }
+
+            [BsonElement("title")]
+            [BsonRequired()]
+            public string Title { get; set; }
+
+            [BsonElement("body")]
+            [BsonRequired()]
+            public string Body { get; set; }
+
+            [BsonElement("criado")]
+            [BsonRequired()]
+            public DateTime Criado { get; set; }
+
+            [BsonElement("active")]
+            [BsonRequired()]
+            public bool Active { get; set; }
+
+            [BsonElement("value")]
+            [BsonRequired()]
+            public double Value { get; set; }
+
+            [BsonElement("position")]
+            [BsonRequired()]
+            public int Position { get; set; }
+        }
+}
